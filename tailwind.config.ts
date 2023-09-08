2c81f6619543cd749b2ed3bf6f3bb4721509dac9
Sebastian <sebastian.zawolanski@gmail.com>
2023-09-08 18:36:04 +0200

    Adjust initial configuration to the project needs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,15 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      page: '#E0E8FF',
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+        'page-mobile': 'url(../../public/pattern-background-mobile.svg)',
+        'page-desktop': 'url(../../public/pattern-background-desktop.svg)'
+      }
+    }
   },
   plugins: [],
 }
